3f064d5a29f4cb420beb9d4a25f07d0b7fe94287
André Mello <104536338+aabm007@users.noreply.github.com>
2023-10-30 11:04:33 -0300

    Ex001-Javascript

---
+++ Javascript/Ex001/index.js
@@ -0,0 +1,16 @@
+function register() {
+    let name = document.querySelector("#name").value; // stri
+    let wage = Number(document.querySelector("#wage").value); // number
+    let age = Number(document.querySelector("#age").value);// number
+    let degree = document.querySelector("input[name='degree']:checked").value; // string
+    let res = document.querySelector("#res");
+  
+    res.innerHTML = `
+                      <hr>
+                      <p>Nome: ${name}</p>
+                      <p>Salário: R$ ${wage}</p>
+                      <p>Idade: ${age} anos</p>
+                      <p>Possui diploma: ${degree}</p>
+                      
+    `;
+  }
