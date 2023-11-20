f98e3a857e925fc2f33f9143b42a560a746e8461
André Mello <104536338+aabm007@users.noreply.github.com>
2023-11-20 10:43:49 -0300

    Ex008 Atualização

--- Javascript/Ex008/main.js
+++ Javascript/Ex008/main.js
@@ -1,15 +1,25 @@
 try {
-    let numero = prompt("Informe um número:");
-  
-    let numeroConvertido = Number(numero);
-  
-    if (isNaN(numeroConvertido)) {
-      throw new Error("Você não informou um número válido.");
-    }
-  
-    console.log("Número informado:", numeroConvertido);
-  } catch (erro) {
-    console.error("Ocorreu um erro:", erro.message);
-  } finally {
-    console.log("Bloco finally: Este bloco é executado sempre.");
+  let numero1 = prompt("Informe o primeiro número:");
+  let numero2 = prompt("Informe o segundo número:");
+  let numero3 = prompt("Informe o terceiro número:");
+
+  numero1 = parseFloat(numero1);
+  numero2 = parseFloat(numero2);
+  numero3 = parseFloat(numero3);
+
+  if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) {
+
+    throw new Error("Você deve informar três números válidos.");
+    
   }
+
+  const soma = numero1 + numero2 + numero3;
+
+  console.log("A soma dos números é:", soma);
+} catch (erro) {
+
+  console.error("Ocorreu um erro:", erro.message);
+} finally {
+
+  console.log("Bloco finally: Este bloco é executado sempre.");
+}
